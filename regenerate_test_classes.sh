1aae25fe4815f738a5670561bfe5e8a4915ff68a
Patrik Lilja <lilja.patrik@gmail.com>
2019-03-09 09:27:23 +0100

    Change version number

--- regenerate_test_classes.sh
+++ regenerate_test_classes.sh
@@ -3,8 +3,8 @@ rm $(dirname $0)/integrationtests/src/test/java/dbtests/framework/*
 rm $(dirname $0)/generated-different-configs/features-turned-off/src/test/java/db/h2/*
 rm $(dirname $0)/generated-different-configs/features-turned-off/src/test/resources/*
 find -E $(dirname $0)/integrationtests/src/test/java/dbtests -regex ".*Repository.java|.*Entity.java|.*Repo.java|.*Dao.java" | xargs rm
-java -jar $(dirname $0)/daogenerator/target/daogenerator-0.1-spring-boot.jar postgres-test/postgres.properties
-java -jar $(dirname $0)/daogenerator/target/daogenerator-0.1-spring-boot.jar mysql-test/mysql.properties
-java -jar $(dirname $0)/daogenerator/target/daogenerator-0.1-spring-boot.jar mssql-test/mssql.properties
-java -jar $(dirname $0)/daogenerator/target/daogenerator-0.1-spring-boot.jar oracle-test/oracle.properties
-java -jar $(dirname $0)/daogenerator/target/daogenerator-0.1-spring-boot.jar generated-different-configs/features-turned-off/db.properties
+java -jar $(dirname $0)/daogenerator/target/daogenerator-0.9.0-spring-boot.jar postgres-test/postgres.properties
+java -jar $(dirname $0)/daogenerator/target/daogenerator-0.9.0-spring-boot.jar mysql-test/mysql.properties
+java -jar $(dirname $0)/daogenerator/target/daogenerator-0.9.0-spring-boot.jar mssql-test/mssql.properties
+java -jar $(dirname $0)/daogenerator/target/daogenerator-0.9.0-spring-boot.jar oracle-test/oracle.properties
+java -jar $(dirname $0)/daogenerator/target/daogenerator-0.9.0-spring-boot.jar generated-different-configs/features-turned-off/db.properties
